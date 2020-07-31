e9585dce4e35782c2701b7cd1757b6bbbe405ac6
DorkyTay <67008340+DorkyTay@users.noreply.github.com>
2020-07-30 22:40:13 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LAB_3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string blackListWord = "Fudge";
+            bool isSpam = false;
+
+            string message = Console.ReadLine();
+
+            if (message.Contains(blackListWord))
+            {
+                isSpam = true;
+                Console.WriteLine("The message is spam.");
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("The message is not spam.");
+            }
+
+        }
+    }
+}
